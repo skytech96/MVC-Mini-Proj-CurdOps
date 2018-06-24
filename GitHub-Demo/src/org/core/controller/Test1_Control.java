d0da9d7bca742c8b8b334e1fb249904e2f6b994f
SkyTech <SkyTech@SkyTech>
2018-06-24 15:53:04 +0530

    change 6 added comment to test1control

--- GitHub-Demo/src/org/core/controller/Test1_Control.java
+++ GitHub-Demo/src/org/core/controller/Test1_Control.java
@@ -5,7 +5,7 @@ import org.core.cfgs.Test2;
 public class Test1_Control {
 
 	public static void main(String[] args) {
-	
+		//create object
 		Test2 t = new Test2();
 		System.out.println(t.area(5));
 		System.out.println("github is chutiya");
