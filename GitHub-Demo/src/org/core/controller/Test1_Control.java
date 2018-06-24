738409aedd7aa329ea2784af67a6c8930a89bb3a
SkyTech <SkyTech@SkyTech>
2018-06-24 18:14:20 +0530

    last commit before the first release

--- GitHub-Demo/src/org/core/controller/Test1_Control.java
+++ GitHub-Demo/src/org/core/controller/Test1_Control.java
@@ -7,8 +7,7 @@ public class Test1_Control {
 	public static void main(String[] args) {
 		//create object
 		Test2 t = new Test2();
-
 		System.out.println(t.area(5));
-		System.out.println("infinity war 2 is coming");
+		System.out.println("infinity war 2 is coming...2019");
 	}
 }
